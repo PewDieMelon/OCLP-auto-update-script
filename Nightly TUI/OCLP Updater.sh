639683fdf22cdcb6f9fed217cf4d6037fc8711b8
PewDieMelon <74740271+PewDieMelon@users.noreply.github.com>
2021-10-10 16:07:14 +0200

    added first script

---
+++ Nightly TUI/OCLP Updater.sh
@@ -0,0 +1,9 @@
+## Updater for OCLP TUI Nightly
+## Needs to be set up to auto run
+cd /Users/dennis/.tmp
+wget https://nightly.link/dortania/OpenCore-Legacy-Patcher/workflows/build-app/main/OpenCore-Patcher-TUI.app.zip
+unzip -o OpenCore-Patcher-TUI.app.zip
+unzip -o OpenCore-Patcher-TUI.app.zip
+rm -rf /Applications/OpenCore-Patcher.app
+mv -f OpenCore-Patcher.app /Applications
+rm -rf OpenCore-Patcher-TUI.app.zip*
