0f83a25db7a8e8164693c9d116fcd5674e737e0e
PewDieMelon <74740271+PewDieMelon@users.noreply.github.com>
2021-10-10 16:31:32 +0200

    Added GUI version

---
+++ Nightly GUI/OCLP Updater.sh
@@ -0,0 +1,9 @@
+## Updater for OCLP TUI Nightly
+## Needs to be set up to auto run
+cd /Users/dennis/.tmp
+wget https://nightly.link/dortania/OpenCore-Legacy-Patcher/workflows/build-gui/main/OpenCore-Patcher-GUI.app.zip
+unzip -o OpenCore-Patcher-GUI.app.zip
+unzip -o OpenCore-Patcher-GUI.app.zip
+rm -rf /Applications/OpenCore\ Patcher.app
+mv -f OpenCore\ Patcher.app /Applications
+rm -rf OpenCore-Patcher-GUI.app.zip*
